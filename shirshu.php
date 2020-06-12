cc1bb5bcc2a1ec369832de03cf19221a31f81c02
j-ka11 <joshuakall@utexas.edu>
2020-06-12 21:43:45 +0000

    Created array of supported plugins and got list of plugins in use

--- shirshu.php
+++ shirshu.php
@@ -9,4 +9,19 @@ function SS(){
     return Shirshu::getInstance();
 }
 
+if(!function_exists('get_plugins')){
+    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+
+function check(){
+    $plugins = get_plugins();
+    $plugin_keys = array_keys($plugins);
+    for($i = 0;$i < count($plugin_keys);$i++){
+        echo $plugins[$plugin_keys[$i]]['Name'];
+        echo '<br>';
+    }
+    //fail('fail');
+}
+add_action('plugins_loaded', 'check');
+
 $GLOBALS['shirshu'] = SS();
--- src/Shirshu.php
+++ src/Shirshu.php
@@ -1,14 +1,52 @@
 <?php
 
+/**
+ * Main Shirshu class
+ *
+ * @class Shirshu
+ */
 final class Shirshu{
 
+    /**
+     * List of plugins Shirshu supports for event tracking
+     *
+     * @var array of strings
+     */
+    private $supportedPlugins;
+
+    protected function getSupportedPlugins(){
+        return $this->supportedPlugins;
+    }
+
+    /**
+     * Single instance of the class
+     *
+     * @var Shirshu
+     */
     protected static $instance = null;
 
+    /**
+     * Returns the main Shirshu instance
+     *
+     * @see SS()
+     * @return Shirshu - Main instance
+     */
     public static function getInstance(){
         if(is_null(self::$instance)){
             self::$instance = new self();
         }
         return self::$instance;
+    }
+
+    /**
+     * Create array of supported plugins that Shirshu currently supports
+     */
+    private function defineSupportedPlugins(){
+        $this->supportedPlugins = array('Contact Form 7', 'Formidable Forms', 'Ninja Forms', 'WooCommerce', 'WPForms', 'WPForms Lite');
+    }
+
+    private function __construct(){
+        $this->defineSupportedPlugins();
     }
 
 }
