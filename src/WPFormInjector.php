e17e6b8f82763d7a88c2f444928d696cf2955c1c
Yizhuo Du <michael03160525@gmail.com>
2020-06-16 21:11:52 +0000

    Created the WP Forms Injector class

--- src/WPFormInjector.php
+++ src/WPFormInjector.php
@@ -3,5 +3,26 @@
 
 class WPFormInjector
 {
+    private $eventList = null;
 
+    private $event_submitForm =
+        '<script>
+                if (document.querySelector("button.wpforms-submit") != null) {
+                    alert("detected wp");
+                    document.querySelector("button.wpforms-submit").addEventListener("click", function() {
+                        alert("WPForms: Form Submitted");
+                    });
+                }
+         </script>';
+
+    public function __construct() {
+        $this->eventList = array();
+        array_push($this->eventList, $this->event_submitForm);
+    }
+
+    public function injectCode() {
+        foreach ($this->eventList as $event) {
+            echo $event . '<br>';
+        }
+    }
 }
