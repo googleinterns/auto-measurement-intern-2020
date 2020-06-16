56ec825abbb583bd2593b526368fe144da4201e5
Yizhuo Du <michael03160525@gmail.com>
2020-06-16 21:11:22 +0000

    Created the Ninja Forms Injector class

--- src/NinjaFormInjector.php
+++ src/NinjaFormInjector.php
@@ -3,5 +3,43 @@
 
 class NinjaFormInjector
 {
+    private $eventList = null;
 
+
+
+    private $event_submitForm =
+        '<script>
+//                jQuery(function($){
+//                    document.querySelectorAll(\'div.nf-field-container.submit-container [type="button"]\')[0].addEventListener("click", function() {
+//                        alert("Ninja Forms: Form Submitted");
+//                    });
+//                })
+//                if (document.querySelectorAll(\'div.nf-field-container.submit-container [type="button"]\') != null) {
+//                    alert("detected nj");
+//                    document.querySelectorAll(\'div.nf-field-container.submit-container [type="button"]\')[0].addEventListener("click", function() {
+//                        alert("Ninja Forms: Form Submitted");
+//                    });
+//                } else {
+//                    alert(\'Did not detect nj\');
+//                }
+
+                  jQuery(document).on("nfFormReady", function(e, layoutView) {
+                                       document.querySelectorAll(\'div.nf-field-container.submit-container [type="button"]\')[0].addEventListener("click", function() {
+                      alert("Ninja Forms: Form Submitted");                   });
+   
+                  } );
+                  
+
+         </script>';
+
+    public function __construct() {
+        $this->eventList = array();
+        array_push($this->eventList, $this->event_submitForm);
+    }
+
+    public function injectCode() {
+        foreach ($this->eventList as $event) {
+            echo $event . '<br>';
+        }
+    }
 }
