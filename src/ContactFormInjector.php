d593e3276befe0b42499cee1c2101b51e38954f4
Yizhuo Du <michael03160525@gmail.com>
2020-06-16 21:08:44 +0000

    Created Contact Form 7 Injector

--- src/ContactFormInjector.php
+++ src/ContactFormInjector.php
@@ -1,7 +1,29 @@
 <?php
 
 
-class ContactFormInjector
-{
+class ContactFormInjector {
 
+
+    private $eventList = null;
+
+    private $event_submitForm =
+        '<script>
+                if (document.querySelector("input.wpcf7-submit") != null) {
+                    alert("detected 7");
+                    document.querySelector("input.wpcf7-submit").addEventListener("click", function() {
+                        alert("Contact Form 7: Form Submitted");
+                    });
+                }
+         </script>';
+
+    public function __construct() {
+        $this->eventList = array();
+        array_push($this->eventList, $this->event_submitForm);
+    }
+
+    public function injectCode() {
+        foreach ($this->eventList as $event) {
+            echo $event . '<br>';
+        }
+    }
 }
