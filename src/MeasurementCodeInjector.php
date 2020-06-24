1cb0a9f3c27b33394559a269642621038f88e6e7
j-ka11 <joshuakall@utexas.edu>
2020-06-24 23:50:50 +0000

    Now listening for all events on the root element.
    Previously, we were attaching event listeners to specific elements. Now
    we listen for all events with the root element, and during the capture
    phase of event propogation so we will always capture it. This makes it
    so that we don't need to worry if the elements are added dynamically
    because we simply listen for all events on the root element. It also
    fixes previous bugs that occurred when an event caused a separate element to be
    reloaded dynamically so the existing event handlers were lost.

--- src/MeasurementCodeInjector.php
+++ src/MeasurementCodeInjector.php
@@ -45,7 +45,7 @@ class MeasurementCodeInjector {
         $this->pluginDetector = new PluginDetector($supportedPlugins);
         $this->eventFactory = MeasurementEventFactory::getInstance();
         add_action('plugins_loaded', array($this, 'setActivePlugins'));
-        add_action('wp_footer', array($this, 'injectEventTracking'), 999);
+        add_action('wp_head', array($this, 'injectEventTracking'), 1);
     }
 
     /**
@@ -69,59 +69,35 @@ class MeasurementCodeInjector {
         }
         ?>
         <script>
-            /**
-             * Keeps track of Ninja forms that have already been tracked
-             *
-             * @type Array of elements
-             */
-            let ninjaFormsAddedNodes = [];
+            function pathHasMatch(e, config) {
+                let elemList = document.querySelectorAll(config.selector);
+                let measurementEvent;
+                for(measurementEvent of elemList) {
+                    if(e.target === measurementEvent) {
+                        return true;
+                    }
 
-            /**
-             * Adds event listener to element that corresponds to the event
-             *
-             * @param config - configuration for a trackable event
-             */
-            function addListeners(config) {
-                let nodeList = document.querySelectorAll(config.selector);
-                for(node of nodeList) {
-                    if(!ninjaFormsAddedNodes.includes(node)) {
-                        node.addEventListener(config.on, function () {
-                            alert('Got an event called: '.concat(config.action));
-                        });
-                        if(config.pluginName == 'Ninja Forms') ninjaFormsAddedNodes.push(node);
+                    let path = e.path;
+                    let pathElem;
+                    for(pathElem of path) {
+                        if(pathElem === measurementEvent) {
+                            return true;
+                        }
                     }
                 }
+                return false;
             }
 
-            /**
-             * Adds first or second layer event listeners on DOM loaded
-             */
-            jQuery(function($){
-                let eventConfigurations = <?php echo json_encode($this->eventConfigurations); ?>;
-                console.log(eventConfigurations);
-                for(config of eventConfigurations) {
-                    if(config.secondLayerOn === null) {
-                        addListeners(config);
-                    }else{
-                        let secondLayerNode;
-                        switch(config.secondLayerSelector) {
-                            case 'document':
-                                secondLayerNode = $(document);
-                                break;
-                            case 'document.body':
-                                secondLayerNode = $(document.body);
-                                break;
-                            default:
-                                secondLayerNode = $(config.secondLayerSelector);
-                                break;
-                        }
-                        const configCopy = JSON.parse(JSON.stringify(config));
-                        secondLayerNode.on(config.secondLayerOn, function(){
-                            addListeners(configCopy);
-                        });
+            let eventConfigurations = <?php echo json_encode($this->eventConfigurations); ?>;
+            let config;
+            for(config of eventConfigurations) {
+                const thisConfig = config;
+                document.addEventListener(config.on, function(e){
+                    if(pathHasMatch(e, thisConfig)) {
+                        alert('Got an event called: '.concat(thisConfig.action));
                     }
-                }
-            });
+                }, true)
+            }
         </script>
         <?php
     }
