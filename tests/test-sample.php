82f51bae06092b914f4c2db9d0a9c9f44d50e866
Yizhuo Du <yizhuodu@google.com>
2020-06-24 18:45:36 +0000

    Set up the Wordpress PHPUnit test environment
     Author:    Yizhuo Du <michael03160525@gmail.com>

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Auto_Measurement_Intern_2020
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
