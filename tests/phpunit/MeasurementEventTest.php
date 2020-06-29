dc8d71d601e5c78811f8a015dbd6390373555b07
Yizhuo Du <michael03160525@gmail.com>
2020-06-29 14:32:55 +0000

    Modify the expected Javascript code for webpage injection

--- tests/phpunit/MeasurementEventTest.php
+++ tests/phpunit/MeasurementEventTest.php
@@ -80,60 +80,18 @@ class MeasurementEventTest extends WP_UnitTestCase {
         $jsonString = json_encode($eventConfigurations);
 
         $expectedString = "<script>
-            /**
-             * Keeps track of Ninja forms that have already been tracked
-             *
-             * @type Array of elements
-             */
-            let ninjaFormsAddedNodes = [];
-
-            /**
-             * Adds event listener to element that corresponds to the event
-             *
-             * @param config - configuration for a trackable event
-             */
-            function addListeners(config) {
-                let nodeList = document.querySelectorAll(config.selector);
-                for(node of nodeList) {
-                    if(!ninjaFormsAddedNodes.includes(node)) {
-                        node.addEventListener(config.on, function () {
-                            alert('Got an event called: '.concat(config.action));
-                        });
-                        if(config.pluginName == 'Ninja Forms') ninjaFormsAddedNodes.push(node);
+            let eventConfigurations = $jsonString;
+            let config;
+            for(config of eventConfigurations) {
+                const thisConfig = config;
+                document.addEventListener(config.on, function(e){
+                    if(e.target.matches(thisConfig.selector)) {
+                        alert('Got an event called: '.concat(thisConfig.action));
+                    }else if(e.target.matches(thisConfig.selector.concat(' *'))){
+                        alert('Got an event called: '.concat(thisConfig.action));
                     }
-                }
+                }, true);
             }
-
-            /**
-             * Adds first or second layer event listeners on DOM loaded
-             */
-            jQuery(function($){
-                let eventConfigurations = $jsonString;
-                console.log(eventConfigurations);
-                console.log(eventConfigurations[0].pluginName);
-                for(config of eventConfigurations) {
-                    if(config.secondLayerOn === null) {
-                        addListeners(config);
-                    }else{
-                        let secondLayerNode;
-                        switch(config.secondLayerSelector) {
-                            case 'document':
-                                secondLayerNode = $(document);
-                                break;
-                            case 'document.body':
-                                secondLayerNode = $(document.body);
-                                break;
-                            default:
-                                secondLayerNode = $(config.secondLayerSelector);
-                                break;
-                        }
-                        const configCopy = JSON.parse(JSON.stringify(config));
-                        secondLayerNode.on(config.secondLayerOn, function(){
-                            addListeners(configCopy);
-                        });
-                    }
-                }
-            });
         </script>";
 
         $this->expectOutputString($expectedString);
